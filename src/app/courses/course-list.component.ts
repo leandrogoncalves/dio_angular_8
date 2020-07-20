75ddad43f3edadac5e2f398a2b8f37cd34004ec1
Leandro <leandro.gsilva@fde.sp.gov.br>
2020-07-19 21:08:50 -0300

    Delentando registros

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -28,6 +28,16 @@ export class CourseListComponent implements OnInit {
     });
   }
 
+  deleteById(courseId: number): void {
+    this.CourseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log('Deleted with success');
+        this.retrieveAll();
+      },
+      error: (err) => console.log('Error', err),
+    });
+  }
+
   set filter(value: string) {
     this._filterBy = value;
     this.filteredCourses = this._courses.filter((course: Course) => {
